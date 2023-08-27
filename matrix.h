c83a71e265802d1afa0c131bc9bae7fcbbc67715
jeslinpjames <jeslinpjames007@gmail.com>
2023-08-27 22:55:25 +0530

    Finished adding constructors

--- matrix.h
+++ matrix.h
@@ -45,20 +45,24 @@
 
 #include <vector>
 #include <initializer_list>
+#include <random> 
+#include <fstream>   
+#include <sstream> 
+using namespace std;
 
 class Matrix {
 private:
-    size_t row;
-    size_t col;
-    std::vector<double> mat_data;
+    size_t r;  
+    size_t c;
+    vector<double> mat_data;
 
 public:
     // Constructors
     Matrix(size_t rows, size_t cols);
-    Matrix(const std::vector<std::vector<double>>& data);
-    Matrix(std::initializer_list<std::initializer_list<double>> data);
-    // Other constructors for random matrices and loading from CSV
-
+    Matrix(const vector<vector<double>>& data);
+    Matrix(initializer_list<initializer_list<double>> data);
+    Matrix(size_t rows, size_t cols, bool useUniform, double mean = 0.0, double stddev = 1.0);
+    Matrix(const string& filename);
     // Overloaded operators
     Matrix operator*(const Matrix& other) const;
     Matrix& operator*=(const Matrix& other);
@@ -71,15 +75,15 @@ public:
     bool operator!=(const Matrix& other) const;
 
     // Member functions
-    std::vector<double> column(size_t col) const;
-    std::vector<double> row(size_t row) const;
+    vector<double> column(size_t col) const;
+    vector<double> row(size_t row) const;
     Matrix sub_matrix(size_t start_row, size_t start_col, size_t end_row, size_t end_col) const;
-    std::pair<size_t, size_t> shape() const;
+    pair<size_t, size_t> shape() const;
     void reshape(size_t new_rows, size_t new_cols);
-    void add_row(const std::vector<double>& new_row);
-    void add_column(const std::vector<double>& new_col);
+    void add_row(const vector<double>& new_row);
+    void add_column(const vector<double>& new_col);
     void remove_column(size_t col);
-    void reorder_column(size_t col, const std::vector<size_t>& order);
+    void reorder_column(size_t col, const vector<size_t>& order);
     void sort_matrix(size_t col);
     Matrix transpose() const;
     static Matrix Id(size_t size);
@@ -87,10 +91,10 @@ public:
     double avg() const;
     void head() const;
     void print() const;
-    void to_csv(const std::string& filename) const;
+    void to_csv(const string& filename) const;
 
     // Non-member functions
-    static Matrix diag_matrix(const std::vector<double>& diag);
+    static Matrix diag_matrix(const vector<double>& diag);
     bool is_triangular() const;
     // Other matrix factorization functions
 
@@ -98,11 +102,92 @@ public:
 
 };
 Matrix::Matrix(size_t rows, size_t cols){
-    row = rows;
-    col = cols;
+    r = rows;  
+    c = cols;  
 }
-Matrix::Matrix(const std::vector<std::vector<double>>& data){
-    
+Matrix::Matrix(const vector<vector<double>>& data){
+    r = data.size();  
+    if(r>0){ 
+        c = data[0].size(); 
+        mat_data.reserve(r*c);
+        for(size_t i =0;i<data.size();++i){
+            const vector<double>& row = data[i];
+            if(row.size()!=c){  
+                throw invalid_argument("Input matrix rows have different sizes");
+            }
+            mat_data.insert(mat_data.end(),row.begin(),row.end());
+        }
+    }else
+    c =0;  
+}
+Matrix:: Matrix(initializer_list<initializer_list<double>> data){
+   r = data.size();  
+    if(r>0){  
+         c = data.begin()->size();  
+         mat_data.reserve(r*c);  
+         for(const auto& row : data){
+              if(row.size()!=c){  
+                throw invalid_argument("Input matrix rows have different sizes");
+              }
+              mat_data.insert(mat_data.end(),row.begin(),row.end());
+         } 
+    } else{
+        c =0;  
+    }
+}
+Matrix::Matrix(size_t rows, size_t cols, bool useUniform, double mean, double stddev) {
+    r = rows;  
+    c = cols;  
+
+    random_device rd;
+    mt19937 generator(rd());
+
+    if (useUniform) {
+        uniform_real_distribution<double> uniformDistribution(0.0, 1.0);
+
+        mat_data.reserve(r * c);  
+        for (size_t i = 0; i < r * c; ++i) {  
+            mat_data.push_back(uniformDistribution(generator));
+        }
+    } else {
+        normal_distribution<double> normalDistribution(mean, stddev);
+
+        mat_data.reserve(r * c);  
+        for (size_t i = 0; i < r * c; ++i) {  
+            mat_data.push_back(normalDistribution(generator));
+        }
+    }
+}
+Matrix::Matrix(const string& filename) {
+    ifstream file(filename);  // Open the file
+
+    if (!file.is_open()) {
+        throw runtime_error("Failed to open CSV file");
+    }
+
+    string line;
+    while (getline(file, line)) {
+        istringstream iss(line);
+        double value;
+        vector<double> rowValues;
+
+        while (iss >> value) {
+            rowValues.push_back(value);
+            if (iss.peek() == ',') {
+                iss.ignore();
+            }
+        }
+
+        if (!rowValues.empty()) {
+            if (c == 0) {  
+                c = rowValues.size();  
+            } else if (rowValues.size() != c) {  
+                throw runtime_error("CSV rows have different sizes");
+            }
+            mat_data.insert(mat_data.end(), rowValues.begin(), rowValues.end());
+            r++;  
+        }
+    }
 }
 
 #endif // MATRIX_H

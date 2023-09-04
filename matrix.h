41e94f8016895ee408f41b119971601860bf6bd3
jeslinpjames <jeslinpjames007@gmail.com>
2023-09-04 19:45:02 +0530

    more functions added

--- matrix.h
+++ matrix.h
@@ -48,6 +48,7 @@
 #include <random> 
 #include <fstream>   
 #include <sstream> 
+#include <algorithm>
 using namespace std;
 
 class Matrix {
@@ -92,7 +93,6 @@ public:
     double sum() const ;
     void head() const ;
     void print() const;
-    void to_csv(const string& filename) const;
     double avg() const ;
     void to_csv(const string& filename) const;
 
@@ -445,7 +445,6 @@ void Matrix::head() const {
         cout << "Matrix is empty." << endl;
         return;
     }
-
     cout << "First row of the matrix:" << endl;
     for (size_t j = 0; j < c; ++j) {
         cout << mat_data[j] << " ";
@@ -539,8 +538,87 @@ Matrix LU_Decomposition(const Matrix& matrix) {
 
     return L;
 }
+std::pair<Matrix, Matrix> QR_factorization() const {
+        Matrix Q(r, c);
+        Matrix R(r, c);
+        std::vector<std::vector<double>> u(r, std::vector<double>(c));
+        std::vector<std::vector<double>> v(r, std::vector<double>(c));
+        for (size_t i = 0; i < r; ++i) {
+            for (size_t j = 0; j < c; ++j) {
+                if (i == j) {
+                    R.mat_data[i * c + j] = 1.0;
+                } else {
+                    R.mat_data[i * c + j] = 0.0;
+                }
+                Q.mat_data[i * c + j] = mat_data[i * c + j];
+            }
+        }
+        for (size_t j = 0; j < c; ++j) {
+            for (size_t k = 0; k < j; ++k) {
+                double dot_product = 0.0;
+                for (size_t i = 0; i < r; ++i) {
+                    dot_product += Q.mat_data[i * c + j] * Q.mat_data[i * c + k];
+                }
 
-// static Matrix Matrix::Id(size_t size){
-    
-// }
+                for (size_t i = 0; i < r; ++i) {
+                    u[i][j] = Q.mat_data[i * c + j] - dot_product * Q.mat_data[i * c + k];
+                }
+            }
+            double norm = 0.0;
+            for (size_t i = 0; i < r; ++i) {
+                norm += u[i][j] * u[i][j];
+            }
+            norm = std::sqrt(norm);
+            for (size_t i = 0; i < r; ++i) {
+                Q.mat_data[i * c + j] = u[i][j] / norm;
+            }
+            for (size_t i = 0; i < r; ++i) {
+                v[i][j] = Q.mat_data[i * c + j];
+            }
+            for (size_t k = j; k < c; ++k) {
+                double dot_product = 0.0;
+                for (size_t i = 0; i < r; ++i) {
+                    dot_product += v[i][j] * mat_data[i * c + k];
+                }
+                for (size_t i = 0; i < r; ++i) {
+                    R.mat_data[j * c + k] = dot_product;
+                }
+            }
+        }
+        return std::make_pair(Q, R);
+    }
+std::pair<Matrix, Matrix> eigen_decomposition() const {
+    Matrix A = *this;
+    Matrix Q(r, c);
+    Matrix R(r, c);
+
+    for (size_t i = 0; i < 50; ++i) {
+        std::pair<Matrix, Matrix> qr = A.qr_factorization();
+        Q = qr.first;
+        R = qr.second;
+        A = R * Q;
+    }
+
+    return std::make_pair(A, Q);
+}
+std::tuple<Matrix, Matrix, Matrix> singular_value_decomposition() const {
+    Matrix A = *this;
+    size_t m = A.shape().first;
+    size_t n = A.shape().second;
+    Matrix U(m, m, true);
+    Matrix S(m, n);
+    Matrix V(n, n, true);
+    for (size_t i = 0; i < 50; ++i) {
+        std::pair<Matrix, Matrix> qr = A.qr_factorization();
+        A = qr.second * qr.first;
+        Matrix Qt = qr.first.T();
+        U *= Qt;
+        if (i > 0) {
+            S *= qr.second;
+        } else {
+            S = qr.second;
+        }
+    }
+    return std::make_tuple(U, S, A.T());
+}
 #endif // MATRIX_H
